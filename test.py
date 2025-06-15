3b5a8300e55602c4470f0d1d48249a6f9e37411b
Dmitry Kabanov <dmitry.kab178@gmail.ru>
2025-06-15 11:27:25 +0500

    vers 2
    Signed-off-by: Dmitry Kabanov <dmitry.kab178@gmail.ru>

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-print("hello!")
-print("add new str")
-print(5+6)
-print(12)
+import json
+
+data = {
+  "id": 100,
+  "name": "Bozon Incorporated",
+  "employees": [{"id": "1", "name": "Александр"}]
+}
+
+with open("json_data.json", "w", encoding="UTF-8") as json_file:
+    json.dump(data, json_file)
